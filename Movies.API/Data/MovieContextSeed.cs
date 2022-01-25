c66ea4208eff3fa174781695c15859b1e2c87d62
Thorunitha <s-s.thorunitha@alstomgroup.com>
2022-01-25 16:46:13 +0530

    Add project files.

---
+++ Movies.API/Data/MovieContextSeed.cs
@@ -0,0 +1,42 @@
+﻿using Movies.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Movies.API.Data
+{
+    public class MovieContextSeed
+    {
+        public static void SeedAsync(MoviesAPIContext moviesContext)
+        {
+            if (!moviesContext.Movie.Any())
+            {
+                var movies = new List<Movie>
+                {
+            new Movie
+            {
+                Id = 1,
+                Genre = "Drama",
+                Title = "The Redemption",
+                Rating = "9.3",
+                ImageUrl = "images/src",
+                ReleaseDate = new DateTime(1994, 5,5),
+                Owner = "alice"
+            },
+            new Movie
+            {
+                Id=2,
+                Genre="Crime",
+                Title="12 Angry Men",
+                Rating="8.9",
+                ImageUrl="images/src",
+                ReleaseDate=new DateTime(1993,5,5),
+                Owner="alice"
+            }
+            };
+                moviesContext.Movie.AddRange(movies);
+                moviesContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Movies.Client/ApiServices/MovieApiService.cs
@@ -0,0 +1,61 @@
+﻿using IdentityModel.Client;
+using Movies.Client.ApiServices;
+using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.ApiServices
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            //// 1. "retrieve" our api credentials. This must be registered on Identity Server!
+            var apiClientCredentials = new ClientCredentialsTokenRequest
+            {
+                Address = "https://localhost:5005/connect/token",
+
+               ClientId = "movieClient",
+               ClientSecret = "secret",
+
+            //    // This is the scope our Protected API requires. 
+             Scope = "movieAPI"
+           };
+            var movieList = new List<Movie>();
+             movieList.Add(
+                 new Movie
+            {
+                Id = 1,
+                Genre = "Drama",
+                Title = "The Redemption",
+                Rating = "9.3",
+                ImageUrl = "images/src",
+                ReleaseDate = new DateTime(1994, 5,5),
+                Owner = "alice"
+            }
+                 );
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
